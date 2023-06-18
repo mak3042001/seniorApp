f051dfec124fe1e1b89115a6d95dcf2137947592
android studio <mak3042001@gmail.com>
2023-06-18 20:25:06 +0300

    done

---
+++ lib/domain/usecase/auth/profile_index_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:senior/data/network/failure.dart';
+import 'package:senior/data/network/request.dart';
+import 'package:senior/domain/model/model.dart';
+import 'package:senior/domain/repository/domain_repository.dart';
+import 'package:senior/domain/usecase/auth/base_usecase.dart';
+
+class ProfileIndexUseCase
+    implements BaseUseCase<void, Auth> {
+  final Repository _repository;
+
+  ProfileIndexUseCase(this._repository ,);
+
+  @override
+  Future<Either<Failure, Auth>> execute(
+      void input) async {
+    return await _repository.profileIndex();
+  }
+}
